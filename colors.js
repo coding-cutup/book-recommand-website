30992f833190fc1f71ad84e1b4fe44cf4d5aae11
임승오 <104992152+coding-cutup@users.noreply.github.com>
2022-06-25 16:23:23 +0900

    JQuery라이브러리를 이용하여 color.js를 간단명료하게 리팩토링

--- colors.js
+++ colors.js
@@ -1,19 +1,22 @@
 var body ={
   SetColor : function(color){/*객체와 객체 사이는 콤마로 구분한다*/
-    document.querySelector('body').style.color=color;
+    // document.querySelector('body').style.color=color;
+    $('body').css('color',color); //  body태그의 글자색을 파라미터값으로 설정
   },
   SetBackgroundColor : function(color){
-    document.querySelector('body').style.backgroundColor=color;
+    // document.querySelector('body').style.backgroundColor=color;
+    $('body').css('backgroundColor',color); //  body태그의 배경색을 파라미터값으로 설정
   }
 }
 var link = {
   SetColor : function(color){
-    var alist =document.querySelectorAll('a');
-    var i=0;
-    while(i<alist.length){
-      alist[i].style.color = color;
-      i+=1;
-    }
+    // var alist =document.querySelectorAll('a');
+    // var i=0;
+    // while(i<alist.length){
+    //   alist[i].style.color = color;
+    //   i+=1;
+    // }
+    $('a').css('color', color);
   }
 }
 function nightDayHandler(self){
